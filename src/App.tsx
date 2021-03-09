2a87dc2dc76da1bab98056ad54be399cf7690858
sherlyfebrianti96 <sherlyfebrianti96@yahoo.co.id>
2021-03-09 16:16:47 +0100

    initiate the static page View

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  render() {
+    return (
+      <div id="container">
+        <div className="row">
+          <div className="col-8 offset-2">
+            <h1>Filterable React List</h1>
+            <form>
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Search..."
+              />
+            </form>
+            <table className='table'>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Phone</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Tom Jackson</td>
+                  <td>555-444-333</td>
+                  <td>tom@gmail.com</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  };
 }
 
 export default App;
